49f28bb4697eb56232cee600302373ee945f1cce
Joao Vitor S <125044493+scapfurado@users.noreply.github.com>
2025-08-08 19:22:38 -0300

    jj

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const simbolos = '!@%#*?';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector(‘.forca’);
+const forcaSenha = document.querySelector('.forca');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
